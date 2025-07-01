a0974da5c696c117cc795d186f81f2559817f12a
Wei Sun <sagasw@gmail.com>
2025-07-01 01:43:21 +0000

    addd

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,7 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
+import remarkMermaid from "astro-diagram/remark-mermaid";
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,6 +19,7 @@ export default defineConfig({
   ],
   markdown: {
     remarkPlugins: [
+      remarkMermaid,
       remarkToc,
       [
         remarkCollapse,
